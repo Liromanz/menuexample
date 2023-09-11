1bb42bbd76b1248701b3e87a6be4d673855bd882
Liromanz <kiss_wiwa_2003@mail.ru>
2023-09-11 13:15:34 +0300

    первый раз все отправила

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int num;
+do
+{
+    Console.WriteLine("Какой ваш любимый цвет?");
+    Console.WriteLine("1. Розовый");
+    Console.WriteLine("2. Фиолетовый");
+    Console.WriteLine("3. Салатовый");
+    Console.WriteLine("4. Черный");
+    Console.WriteLine("5. нкикакой выйди изх программы пж");
+
+    string txt = Console.ReadLine();
+    num = Convert.ToInt32(txt);
+} while (num != 5);
+
